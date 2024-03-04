32212ec7084a20277425e36954d57e9cc4a47804
MavenOwll <luis.flex72@gmail.com>
2024-03-04 17:55:21 -0300

    modified:   autenticacao_project/__pycache__/urls.cpython-311.pyc 	modified:   autenticacao_project/urls.py

--- autenticacao_project/urls.py
+++ autenticacao_project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from autenticacao_app import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/auth/login')),
     path('admin/', admin.site.urls),
     path('auth/', include('autenticacao_app.urls')),
 ]
